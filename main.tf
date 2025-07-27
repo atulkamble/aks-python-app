e72d97d2f159e3257e505a710b5838d88f19b4a8
Atul Kamble <atul_kamble@hotmail.com>
2025-07-27 05:56:03 +0530

    code

---
+++ main.tf
@@ -0,0 +1,50 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "inventoryapp"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "kubernetes_namespace" "app" {
+  metadata {
+    name = "inventory"
+  }
+}
+
+resource "kubernetes_manifest" "deployment" {
+  manifest = yamldecode(file("${path.module}/kubernetes/deployment.yaml"))
+}
+
+resource "kubernetes_manifest" "service" {
+  manifest = yamldecode(file("${path.module}/kubernetes/service.yaml"))
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-aks-inventory"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "aks_cluster_name" {
+  default = "aks-inventory-cluster"
+}
